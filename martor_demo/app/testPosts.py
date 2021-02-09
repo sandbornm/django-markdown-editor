35474bb32d2ac3fdcc4cae36c41dfce0185fe98f
sandbornm <michael.sandborn@vanderbilt.edu>
2021-02-09 10:06:37 -0600

    unit test and static files'

---
+++ martor_demo/app/testPosts.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from app.models import Post
+
+class PostTestCase(TestCase):
+    def testPost(self):
+        post = Post(title="title", description="blurba", wiki="body")
+        self.assertEqual(post.title, "title")
+        self.assertEqual(post.description, "blurba")
+        self.assertEqual(post.wiki, "body")
