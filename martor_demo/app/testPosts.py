95b3427fca8967aa26ce4a4730845194891e6c62
sandbornm <michael.sandborn@vanderbilt.edu>
2021-02-11 09:53:22 -0600

    obey the linter

--- martor_demo/app/testPosts.py
+++ martor_demo/app/testPosts.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from app.models import Post
 
 class PostTestCase(TestCase):
+    
+    
     def testPost(self):
         post = Post(title="title", description="blurba", wiki="body")
         self.assertEqual(post.title, "title")
